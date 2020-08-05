d319ef0dbe922c911f397af53d7fd53d629da4a0
bradley-cho <64006888+bradley-cho@users.noreply.github.com>
2020-08-05 13:00:41 +0900

    0.2

--- tests/testSteps/placement_test.js
+++ tests/testSteps/placement_test.js
@@ -3,7 +3,7 @@ module.exports = function() {
       //include group 입력
       includeGroup: function(includeGroupName) {
         // this.selectOption("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > input","#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__content-wrapper > ul > li:nth-child(2) > span > span")
-         this.fillField("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > input",includeGroupName)
+         this.fillField("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-chil  d(2) > div > div > div.multiselect__tags > input",includeGroupName)
          this.waitForElement(includeGroupName)
          this.pressKey(['Enter'])
         this.wait(10)
