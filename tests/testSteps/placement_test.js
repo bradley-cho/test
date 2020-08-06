b276aa0e9e3c1fb46078be876daa2a591ef34460
bradley-cho <64006888+bradley-cho@users.noreply.github.com>
2020-08-06 14:05:46 +0900

    k

--- tests/testSteps/placement_test.js
+++ tests/testSteps/placement_test.js
@@ -5,7 +5,7 @@ module.exports = function() {
         //this.selectOption("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > input","#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__content-wrapper > ul > li:nth-child(2) > span > span")
         
         this.click("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > span")
-        this.fillField("#adForddm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > input",includeGroupName)
+        this.fillField("#adForm > div > div:nth-child(5) > div > div > div > div > div.group-unit__container > div:nth-child(2) > div > div > div.multiselect__tags > input",includeGroupName)
         this.wait(3)
         //this.waitForElement(includeGroupName)
         this.pressKey(['Enter'])
